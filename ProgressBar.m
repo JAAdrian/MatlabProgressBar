9692feac4db9bf6d0faafb915e524c7076b53ff4
Jens-Alrik Adrian <jens-alrik.adrian@jade-hs.de>
2016-06-20 16:37:45 +0200

    Input parsing in constructor, static obj. list, update fnc

--- ProgressBar.m
+++ ProgressBar.m
@@ -20,52 +20,148 @@ classdef ProgressBar < handle
 
 
 properties (Access = private)
-
+    bar = '';
+    IterationCounter = 1;
 end
 
 
 properties (SetAccess = private, GetAccess = public)
-
+    Title = '';
+    Total;
+    Unit = '';
 end
 
 
 properties (Access = public)
-    Title = '';
-    minIteration = 0;
-    maxIteration;
+    UpdateRate;
 end
 
 
 
 methods
-    function [self] = ProgressBar(numIterations, minIterations)
+    function [self] = ProgressBar(total, varargin)
         if ~nargin,
            return;
         end
         
-        if nargin < 2 || isempty (minIterations),
-            minIterations = 1;
-        end
+        % parse input arguments
+        self.parseInputs(total, varargin{:});
+        
         
         % see if there is already a timer object and register the new
         % ProgressBar object
+        objList = self.getObjectList();
+        if isempty(objList),
+            t = timer();
+            self.setObjectList(t);
+        end
         
+    end
+    
+    function [] = update(self, n, wasSuccessful)
+        if nargin < 3 || isempty(wasSuccessful),
+            wasSuccessful = true;
+        end
+        if nargin < 2 || isempty(n),
+            n = 1;
+        end
+        validateattributes(n, ...
+            {'numeric'}, ...
+            {'scalar', 'positive', 'real', 'nonnan', 'finite', 'nonempty'} ...
+            );
+        validateattributes(wasSuccessful, ...
+            {'logical', 'numeric'}, ...
+            {'scalar', 'binary', 'nonnan', 'nonempty'} ...
+            );
         
+        self.incrementIterationCounter(n);
+        
+        self.printProgressBar();
+    end
+    
+    function [] = printMessage(self)
+        
+    end
+    
+    function [] = close(self)
+        
+    end
+    
+    function [] = delete(self)
+        self.close();
     end
 end
 
 
 methods (Access = private)
-%     function [] = printHeader()
-%         fprintf(1, );
-%     end
-%
-%     function [] = printSection()
-%         fprintf(1, );
-%     end
+    function [] = parseInputs(self, total, varargin)
+        p = inputParser;
+        p.FunctionName = mfilename;
+        
+        p.addRequired('total', ...
+            @(in) validateattributes(in, ...
+            {'numeric'}, ...
+            {'scalar', 'integer', 'positive', 'real', 'nonnan', 'finite'} ...
+            ) ...
+            );
+        
+        p.addParameter('Unit', 'Integers', ...
+            @(in) any(validatestring(in, {'Integers', 'Bytes'})) ...
+            );
+        p.addParameter('Title', '', ...
+            @(in) validateattributes(in, {'char'}, {'nonempty'}) ...
+            );
+        
+        p.parse(total, varargin{:});
+        
+        % grab inputs
+        self.Total = p.Results.total;
+        self.Unit = p.Results.Unit;
+        self.Title = p.Results.Title;
+    end
+    
+    function [] = printTitle(self)
+        fprintf(1, '', self);
+    end
+    
+    function [] = setupBar(self)
+        self.bar = sprintf('x', self);
+    end
+    
+    function [] = printProgressBar(self)
+        t = self.getTimer();
+        
+        fprintf(1, 'x', self);
+    end
+    
+    function [] = incrementIterationCounter(self, n)
+        self.IterationCounter = self.IterationCounter + n;
+    end
+    
+    function [] = setObjectList(self, newObj) %#ok<INUSL>
+        ProgressBar.objectList(newObj);
+    end
+    
+    function [list] = getObjectList(self) %#ok<MANU>
+        list = ProgressBar.objectList();
+    end
+    
+    function [timerObject] = getTimer(self) %#ok<MANU>
+        timerObject = self.getObjectList();
+        timerObject = timerObject{1};
+    end
+end
 
-    function [] = printDashes(numDashes)
-        fprintf(1, repmat(self.dashSymbol, 1, numDashes));
+methods (Access = private, Static = true)
+    function list = objectList(newObject)
+        % http://stackoverflow.com/a/14571266
+        persistent ProgObjects;
+        
+        if nargin,
+            ProgObjects = [ProgObjects; {newObject}];
+        end
+        
+        list = ProgObjects;
     end
 end
 
@@ -76,6 +172,31 @@ end
 
 
 end
+
+function [thisBlock] = getBlock(idx)
+% idx ranges from 1 to 9, since the HTML 'left blocks' range from 1 to 8
+% excluding the 'space' but this function also returns the space as first
+% block
+
+blocks = [
+    blanks(1);
+    char(9615);
+    char(9614);
+    char(9613);
+    char(9612);
+    char(9611);
+    char(9610);
+    char(9609);
+    char(9608);
+    ];
+
+thisBlock = blocks(idx);
+end
+
+function [] = backspace(numChars)
+fprintf(1, repmat('\b', 1, numChars));
+end
+
 
 
 
